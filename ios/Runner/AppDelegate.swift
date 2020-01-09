ce5ae3a89ab2bd59a4a3f7d12e29b9282613b833
songkyung <min.songkyung@gmail.com>
2020-01-09 11:11:53 +0900

    native connect with ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let controller = self.window.rootViewController as! FlutterViewController
+    
+    let channel = FlutterMethodChannel.init(name: "example.com/value", binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler { (call, result) in
+        if (call.method == "getValue") {
+            return result("성공")
+        } else {
+            result(FlutterMethodNotImplemented)//구현되어있지 않음
+        }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
